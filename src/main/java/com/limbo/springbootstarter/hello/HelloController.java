ffd6fb54b973f290b8bfcb141ae73a7d33ee2c1f
limbo93 <mhriad0@gmail.com>
2018-01-17 09:55:44 +0600

    Intigrated with swagger2-initial code

---
+++ src/main/java/com/limbo/springbootstarter/hello/HelloController.java
@@ -0,0 +1,16 @@
+package com.limbo.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@GetMapping
+	public String sayHi() {
+		return "Hi";
+	}
+}
